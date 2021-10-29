1c2884193ec022ad3eaa115f7f7f1b5e7adf9e9d
ikerhernandez <74660601+ikerhernandez@users.noreply.github.com>
2021-10-29 10:30:58 +0200

    start

---
+++ model/ProductosClass.php
@@ -0,0 +1,74 @@
+<?php
+    
+class ProductosClass {
+    
+    public $id;
+    public $nombre;
+    public $precio;
+    public $stock;
+    
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @param mixed $precio
+     */
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+
+    /**
+     * @param mixed $stock
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+    }
+    
+}
---
+++ model/connect_data_SERV.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class connect_data
+{
+    public $host='localhost';
+    public $userbbdd='lauzeru5_root';
+    public $passbbdd='Taldea4zerbitzaria';
+    public $ddbbname='lauzeru5_banco';
+}
+
