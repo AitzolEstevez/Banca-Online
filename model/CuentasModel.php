bc8677ec523e91cd4419080ebd7d381bcccfd2f4
phorcajada <p_horcajada@fpzornotza.com>
2021-11-02 09:14:08 +0100

    cuentasmodel opne close vacio

--- model/CuentasModel.php
+++ model/CuentasModel.php
@@ -7,3 +7,19 @@ if($_SERVER['SERVER_NAME']=="lau.zerbitzaria.net"){
 }
 
 include_once("CuentasClass.php");
+
+class CuentasModel extends CuentasClass{
+
+    public function OpenConnect(){
+
+    }
+
+    public function CloseConnect()
+    {
+        //mysqli_close ($this->link);
+    }
+
+
+
+
+}
