1c0c44c4f87ed86c8ed2bcaef2cffc1f40bdf5af
phorcajada <p_horcajada@fpzornotza.com>
2021-11-02 10:13:26 +0100

    models open/close vacio

--- model/UsuariosModel.php
+++ model/UsuariosModel.php
@@ -6,3 +6,17 @@ if($_SERVER['SERVER_NAME']=="lau.zerbitzaria.net"){
 }else{
     include_once ("connect_data_LOCAL.php");
 }
+
+
+
+class UsuariosModel extends UsuariosClass{
+
+    public function OpenConnect(){
+
+    }
+
+    public function CloseConnect()
+    {
+        //mysqli_close ($this->link);
+    }
+}
