d350b0b039644f593f120919ee779109ef64f013
ikerhernandez <74660601+ikerhernandez@users.noreply.github.com>
2021-11-04 13:00:39 +0100

    cambios

---
+++ controller/controller_Extracto.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once ("../model/ExtractoModel.php");
+
+$extracto= new ExtractoModel();
+
+$response=array();
+
+$response['listClientes']=$extracto->setListClientes();
+$response['listProveedores']=$extracto->setListProveedores();
+$response['error']="no error";
+
+echo json_encode($response);
+
+unset ($extracto);
+unset ($response);
