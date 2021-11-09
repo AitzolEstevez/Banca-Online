aec15dbac3cafb64958d8c1eb6ec4be85333b0a1
AitzolEstevez <74302566+AitzolEstevez@users.noreply.github.com>
2021-11-09 12:46:52 +0100

    cositas

--- view/js/admin-banca.js
+++ view/js/admin-banca.js
@@ -79,3 +79,11 @@ function Cancelar(){
       )
       
 }
+function onlyNumberKey(evt) {
+          
+  // Only ASCII character in that range allowed
+  var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+      return false;
+  return true;
+}
