f58d1b91cab53ded0a3ab57e7367cc6f81a79ca6
ikerhernandez <74660601+ikerhernandez@users.noreply.github.com>
2021-11-11 12:41:22 +0100

    uft

--- view/js/admin-banca.js
+++ view/js/admin-banca.js
@@ -129,13 +129,19 @@ function loadExtracto(){
 	                  +"<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>"
 	                +"</div>"
 	                +"<div class='modal-body'>"
+					+"<div style='display:flex;'>"
+					  +"<div style='width:40%; padding:10px;'>"
+					  +"<img id='modalImg' width='100%' height='100%' src='https://d.newsweek.com/en/full/1064234/base-goku-dramatic-finish.jpg?w=1600&h=1200&q=88&f=bde9c6b36d3234f7b7e7e898f29aa21a' alt=''>"
+					  +"</div>" 
+					  +"<div style='width:60%; padding:10px;'>"
 					  +"<h3>Cuenta</h3>"
 					  +"<div id='Cuentas2'></div>"
 	                  +"<h3>Proveedor</h3>"
 					  +"<div id='Proveedores2'></div>"
-					  +"<img id='modalImg' width='100px' height='100px' src='https://d.newsweek.com/en/full/1064234/base-goku-dramatic-finish.jpg?w=1600&h=1200&q=88&f=bde9c6b36d3234f7b7e7e898f29aa21a' alt=''>"
 					  +"<h3>Producto</h3>"
 					  +"<div id='Productos'></div>"
+					  +"</div>"
+					+"</div>"
 	                  +"<div id='modalFlex'><div>"
 					  +"<h3>Precio/Ud</h3>"
 					  +"<input id='Precio' type='text' disabled class='form-control' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm'>"
@@ -163,6 +169,10 @@ function loadExtracto(){
 						Cancelar();
 					});
 
+					document.querySelector(".btn-close").addEventListener("click",function(){
+						Cancelar();
+					});
+
 					/*document.getElementById("btnPedido").addEventListener("click",function(){
 						Confirmacion();
 					});*/
@@ -170,7 +180,7 @@ function loadExtracto(){
 					document.getElementById("btnProveedor").addEventListener("click",function(){
 						
 						var newRow ="";
-						newRow += "<select class='modalCombo' id='SelectCuentas2' class='form-select' aria-label='Default select example'>";
+						newRow += "<select class='modalCombo' id='SelectCuentas2' style='width:100%;' class='form-select' aria-label='Default select example'>";
 						newRow +="<option selected value=-1>Selecciona una cuenta</option>";
 
 						for (let i = 0; i < cuentas.length; i++) {
@@ -199,7 +209,7 @@ function loadExtracto(){
 							var proveedor = result.listProveedores;
 
 							var newRow ="";
-							newRow += "<select class='modalCombo' id='SelectProveedor' class='form-select' aria-label='Default select example'>";
+							newRow += "<select class='modalCombo' id='SelectProveedor' style='width:100%;' class='form-select' aria-label='Default select example'>";
 							newRow +="<option selected value=-1>Selecciona un proveedor</option>";
 
 							
@@ -217,7 +227,7 @@ function loadExtracto(){
 						.catch(error => console.error('Error status:', error));	
 
 							var newRow ="";
-							newRow += "<select class='modalCombo' id='SelectProducto' class='form-select' aria-label='Default select example'>";
+							newRow += "<select class='modalCombo' id='SelectProducto' style='width:100%;' class='form-select' aria-label='Default select example'>";
 							newRow +="<option selected value=-1>Selecciona un producto</option>";
 
 							newRow +="</select>";
@@ -252,7 +262,7 @@ function loadExtracto(){
 								var productos = result.listProductos;
 							
 								var newRow ="";
-								newRow += "<select class='modalCombo' id='SelectProducto' class='form-select' aria-label='Default select example'>";
+								newRow += "<select class='modalCombo' id='SelectProducto' style='width:100%;' class='form-select' aria-label='Default select example'>";
 								newRow +="<option selected value=-1>Selecciona un producto</option>";
 	
 								
