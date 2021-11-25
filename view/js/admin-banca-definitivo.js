519efa29f2847366aeedbc32ab15757f61e0d43c
AitzolEstevez <74302566+AitzolEstevez@users.noreply.github.com>
2021-11-25 08:47:17 +0100

    Solo numeros

--- view/js/admin-banca-definitivo.js
+++ view/js/admin-banca-definitivo.js
@@ -331,3 +331,14 @@ function loadLeasing(){
 
 
 }
+
+/*Solo Numeros
+------------------------------------------------------------------------------------------------*/
+function onlyNumberKey(evt) {
+          
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+  }
